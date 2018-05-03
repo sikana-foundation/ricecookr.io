a6f0a916bee2458e7271eb5f04295b714ea3eb21
Quentin Barbosa <quentin@sikana.tv>
2018-05-03 18:15:03 +0200

    Adding Arabic to languages

--- uploadchannels.py
+++ uploadchannels.py
@@ -9,7 +9,7 @@ with open("parameters.yml", "r") as f:
 
 # Sikana's API access
 KOLIBRI_TOKEN = parameters["kolibri"]["token"]
-LANGUAGES = ["en", "fr", "es", "pt", "pt-br", "pl", "tr", "ru", "zh", "zh-tw"]
+LANGUAGES = ["en", "fr", "es", "pt", "pt-br", "pl", "tr", "ru", "zh", "zh-tw", "ar"]
 
 for ln in LANGUAGES:
     os.system("python3 -m ricecooker uploadchannel \"ricecookr.py\" --publish --token={} language_code={}".format(KOLIBRI_TOKEN, ln))
